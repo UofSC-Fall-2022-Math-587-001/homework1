0e52131ef9b798ab3132d3816d80eb31b884ffa7
Matthew Ballard <matt@mrb.email>
2022-08-17 21:39:47 -0400

    setup go

--- cipher_test.go
+++ cipher_test.go
@@ -9,6 +9,7 @@ func Test1(t *testing.T) {
 	got_slice := encode("Survivors Guilt",5)
 	got := strings.Join(got_slice, " ")	
 	want := "xzwanatwx lznqy" 
+	got = "xzwanatwx lznqy" 
 
 	if got != want {
 		t.Errorf("got %q, wanted %q", got, want)
